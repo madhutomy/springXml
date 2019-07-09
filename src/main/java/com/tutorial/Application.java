58d125f3f6c786e44f6fc3d67b469e6a5cf3804f
madhutomyhsf <madhutomy@hsfranchise.com>
2019-07-09 11:14:45 -0500

    Constructor injection example

--- src/main/java/com/tutorial/Application.java
+++ src/main/java/com/tutorial/Application.java
@@ -1,6 +1,8 @@
 package com.tutorial;
 
+import com.tutorial.model.Employee;
 import com.tutorial.service.CustomerService;
+import com.tutorial.service.EmployeeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,5 +14,8 @@ public class Application {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(customerService.findAll().get(0).getFirstName());
+        EmployeeService employeeService = applicationContext.getBean("employeeService", EmployeeService.class);
+        Employee employee = employeeService.findAll().get(0);
+        System.out.println(employee.getName() + ", " + employee.getDept() + ", " + employee.getEmpId());
     }
 }
