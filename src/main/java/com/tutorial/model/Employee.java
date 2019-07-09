b7d10f0fc11f9fb2205531f656e1da139509c612
madhutomyhsf <madhutomy@hsfranchise.com>
2019-07-09 11:12:20 -0500

    Initial commit

---
+++ src/main/java/com/tutorial/model/Employee.java
@@ -0,0 +1,32 @@
+package com.tutorial.model;
+
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int empId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+}
